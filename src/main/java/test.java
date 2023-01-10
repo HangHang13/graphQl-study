2557236640eab86fad1feefa6a2189a767731a54
unknown <dhkdwlsgod@naver.com>
2023-01-10 14:21:56 +0900

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+import com.example.demo.GraphQlApplication;
+import org.springframework.boot.SpringApplication;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+
+    }
+}
